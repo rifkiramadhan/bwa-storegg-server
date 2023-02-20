04f1543290c541e58958488285848b22d2e23a72
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2023-02-20 14:06:21 +0700

    Add strictQuery

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv');
 const path = require('path');
-dotenv.config()
+dotenv.config();
 
 module.exports = {
-    rootPath: path.resolve(__dirname, '..'),
-    serviceName: process.env.SERVICE_NAME,
-    jwtKey: process.env.SECRET,
-    urlDb: process.env.MONGO_URL
-}
+  rootPath: path.resolve(__dirname, '..'),
+  serviceName: process.env.SERVICE_NAME,
+  jwtKey: process.env.SECRET,
+  urlDb: process.env.MONGO_URL,
+};
--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const {urlDb} = require('../config');
+const { urlDb } = require('../config');
 
+mongoose.set('strictQuery', false);
 mongoose.connect(urlDb);
 
 const db = mongoose.connection;
